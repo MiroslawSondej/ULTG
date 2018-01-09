e81b32277047f7fc7b0526f6b4a6376792ad0afa
emes <sondejmiroslaw@gmail.com>
2018-01-09 02:10:18 +0100

    Parallax (?!)

--- core/src/com/mygdx/ultg/game/ParallaxSkybox.java
+++ core/src/com/mygdx/ultg/game/ParallaxSkybox.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 
 import java.util.Queue;
 
@@ -16,6 +17,7 @@ public class ParallaxSkybox {
     float _multiplier;
 
     public ParallaxSkybox(float x, float y, Texture skyboxTexture, float multiplier) {
+
         _lastX = x;
         _lastY = y;
         _multiplier = multiplier;
@@ -39,11 +41,11 @@ public class ParallaxSkybox {
         _lastX = x;
         _lastY = y;
 
-        for(int sy = 0; sy < 3; sy++) {
+        /*for(int sy = 0; sy < 3; sy++) {
             for(int sx = 0; sx < 3; sx++) {
                 _skybox[sy][sx].translate(offsetX, offsetY);
             }
-        }
+        }*/
 
         checkIfNearEdge();
     }
@@ -73,7 +75,6 @@ public class ParallaxSkybox {
                 (y - _skybox[0][2].getHeight() / 2) + PhysicsUtility.convertPixelsToMeters(_skybox[0][2].getHeight())
         );
 
-
         //=========
         // MID-LEFT
         _skybox[1][0].setPosition(
@@ -84,12 +85,16 @@ public class ParallaxSkybox {
         _skybox[1][1].setPosition(
                 (x - _skybox[1][1].getWidth() / 2),
                 (y - _skybox[1][1].getHeight() / 2)
+
         );
         // MID-RIGHT
         _skybox[1][2].setPosition(
                 (x - _skybox[1][2].getWidth() / 2) + PhysicsUtility.convertPixelsToMeters(_skybox[1][2].getWidth()),
                 (y - _skybox[1][2].getHeight() / 2)
         );
+
+        Gdx.app.log("DISTANCE", "" + Vector2.dst(_skybox[1][0].getX(), 0f, _skybox[1][1].getX(), 0f));
+        Gdx.app.log("DISTANCE", "" + Vector2.dst(_skybox[1][1].getX(), 0f, _skybox[1][2].getX(), 0f));
 
         //============
         // BOTTOM-LEFT
@@ -110,11 +115,14 @@ public class ParallaxSkybox {
     }
 
     private void checkIfNearEdge() {
-        if(PhysicsUtility.convertPixelsToMeters(_skybox[1][0].getX()) + PhysicsUtility.convertPixelsToMeters(_skybox[1][0].getWidth()) < _lastX) {
-            Gdx.app.log("SWAP","LeftToRight" );
-            Gdx.app.log("SWAP_", "PosX:" + _skybox[1][0].getX());
-            Gdx.app.log("SWAP_", "Offset:" + PhysicsUtility.convertPixelsToMeters(_skybox[1][0].getWidth()));
-            Gdx.app.log("SWAP_", "LastX:" + _lastX);
+        Gdx.app.log("DISTANCE_TEST0","" + Vector2.dst(_skybox[1][0].getX(), 0f, PhysicsUtility.convertMetersToPixels(_lastX), 0f));
+        Gdx.app.log("DISTANCE_TEST1","" + Vector2.dst(_skybox[1][1].getX(), 0f, PhysicsUtility.convertMetersToPixels(_lastX), 0f));
+        Gdx.app.log("DISTANCE_TEST2","" + Vector2.dst(_skybox[1][2].getX(), 0f, PhysicsUtility.convertMetersToPixels(_lastX), 0f));
+
+        Gdx.graphics.setTitle(Vector2.dst(_skybox[1][0].getX(), 0f, PhysicsUtility.convertMetersToPixels(_lastX), 0f) + "|" + Vector2.dst(_skybox[1][1].getX(), 0f, PhysicsUtility.convertMetersToPixels(_lastX), 0f) + "|" + Vector2.dst(_skybox[1][2].getX(), 0f, PhysicsUtility.convertMetersToPixels(_lastX), 0f));
+
+        if(Vector2.dst(_skybox[1][0].getX(), 0f, PhysicsUtility.convertMetersToPixels(_lastX), 0f) > 1024f) {
+            Gdx.app.log("DEBUG","SWAP" );
 
             Sprite newSkybox[][] = new Sprite[3][3];
 
@@ -130,16 +138,13 @@ public class ParallaxSkybox {
             newSkybox[1][2] = _skybox[1][0];
             newSkybox[2][2] = _skybox[2][0];
 
-            //newSkybox[0][2].setX(_skybox[0][0].getX() + 2 * (PhysicsUtility.convertPixelsToMeters(_skybox[0][0].getWidth())));
+            newSkybox[0][2].setX(newSkybox[0][1].getX() + PhysicsUtility.convertPixelsToMeters(newSkybox[0][1].getWidth()));
+            newSkybox[1][2].setX(newSkybox[1][1].getX() + PhysicsUtility.convertPixelsToMeters(newSkybox[1][1].getWidth()));
+            newSkybox[2][2].setX(newSkybox[2][1].getX() + PhysicsUtility.convertPixelsToMeters(newSkybox[2][1].getWidth()));
 
-            newSkybox[1][2].setX(_skybox[1][2].getX() + (PhysicsUtility.convertPixelsToMeters(_skybox[1][0].getWidth())));
-
-            //newSkybox[2][2].setX(_skybox[2][0].getX() + 2 * (PhysicsUtility.convertPixelsToMeters(_skybox[2][0].getWidth())));
-
-            Gdx.app.log("NEWPOS", "x=" + newSkybox[1][2].getX());
-            _skybox = newSkybox;
-
-            return;
+            _skybox = null;
+            _skybox = newSkybox.clone();
+             return;
         }
     }
 
